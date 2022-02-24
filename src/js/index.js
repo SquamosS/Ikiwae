a9e062d98c1445033710e81ac64c9399a653f4b0
Adipati <squamoss@gmail.com>
2022-02-24 15:51:09 +0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+/* ini nanti dipake buat dom */
+
+// const DUMMY_OBJECT = [
+//   {
+//     nama: "VIMLE",
+//     image: "./",
+//     description: "sofa 3 dudukan, dengan sandaran tangan lebar/hallarp krem",
+//     harga: 8995000,
+//   },
+//   {
+//     nama: "HEMNES",
+//     image: "./",
+//     description: "meja tamu, cokelat muda, 90x90cm",
+//     harga: 2799000,
+//   },
+//   {
+//     nama: "BILLY",
+//     image: "./",
+//     description: "rak buku, hitam-cokelat, 40x28x106cm",
+//     harga: 899000,
+//   },
+//   {
+//     nama: "MALM",
+//     image: "./",
+//     description: "tempat tidur ottoman, hitam-cokelat, 180x200cm",
+//     harga: 8499000,
+//   },
+//   {
+//     nama: "TIMUN",
+//     image: "./",
+//     description: "meja makan, cokelat-tua, 200x200cm",
+//     harga: 999999,
+//   },
+// ];
+
+const product = [
+  {
+    nameProduct: ``,
+    stock: ``,
+    shortDesc: ``,
+    longDesc: ``,
+    category: ``,
+    colorAvailable: ``,
+    rating: ``,
+    price: 0,
+    promo: 0,
+  },
+];
